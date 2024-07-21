14a79303f6dd3b3d8e83ed9b65772f1700f255f0
maozhongyu <crmao@qq.com>
2024-07-21 12:26:03 +0800

    命名空间，基础命令

---
+++ command.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+# 快速体验命令
+kubectl get nodes
+# 创建一个 deployment
+kubectl create deployment nginx --image=nginx:1.14-alpine
+# 暴露端口 ,会自动创建service
+kubectl expose deployment nginx --port=80 --type=NodePort
+kubectl get service
+kubectl get namespace
+kubectl get ns
+# 查看命名空间状态
+kubectl get ns kube-system
+kubectl create namepsace dev
+kubectl get namepsace dev
+
+kubectl delete namepsace dev
+
+# 强制删除Terminating pod
+kubectl delete pod  xxxxx --force
+
+
+
+# 如何看apiVersion
+kubectl api-resources --namepsace=true
+kubectl api-resources --namepsace=false
+
---
+++ namespaces/readme.md
@@ -0,0 +1,17 @@
+
+```shell
+kubectl create -f dev-ns.yaml
+kubectl get -f dev-ns.yaml 
+kubectl get ns dev
+kubectl delete -f dev-ns.yaml
+
+
+kubectl apply -f dev-ns.yaml
+# 查看命名空间描述信息
+kubectl describe namespace dev
+```
+
+
+
+
+
